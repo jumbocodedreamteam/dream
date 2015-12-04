b7410f7d9e0151b77039a93ca83879f140050141
ben95cd <bencd@comcast.net>
2015-12-04 16:43:04 -0500

    Angular stuff

--- App2/profile/ProfileController.js
+++ App2/profile/ProfileController.js
@@ -4,7 +4,53 @@ angular.module('dreamApp.profile')
 ProfileController.$inject = ['$scope'/*,'ProfileService'*/];
 
 function ProfileController($scope) {
+	$scope.mentee = {
+		name: "id:Name",
+		dob: "Date of Birth"
+	};
 	
+	$scope.mentor = {
+		firstname: "First Name",
+		lastname: "Last Name",
+		email: "Email",
+		phone: "Phone",
+		address: "Address"
+	};
 	
+	$scope.contacts = [
+		{
+			name: "Name",
+			relationship: "Choose One",
+			email: "Email"
+		},
+		{
+			name: "Name",
+			relationship: "Choose One",
+			email: "Email"
+		}
+		
+	];
+	
+	$scope.newName = {};
+	$scope.emergEmail = {};
+	
+	$scope.submit = function(){
+		if(document.profileForm.mentorFirst.value != ""){
+			$scope.mentor.firstname = $scope.newFirst;
+		}
+		if (document.profileForm.mentorLast.value != ""){
+			$scope.mentor.lastname = $scope.newLast;
+		}
+		if (document.profileForm.mentorEmail.value != ""){
+			$scope.mentor.email = $scope.newEmail;
+		}
+		if (document.profileForm.mentorPhone.value != ""){
+			$scope.mentor.phone = $scope.newPhone;
+		}
+		if (document.profileForm.mentorAddress.value != ""){
+			$scope.mentor.address = $scope.newAddress;
+		}
+		document.profileForm.reset();
+	}
 	
 }
